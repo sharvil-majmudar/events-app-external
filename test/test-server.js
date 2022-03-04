0c6721e799e14c6134a4928cba4f0073fad6fa1a
suneel107 <suneelchintam@gmail.com>
2022-03-04 10:09:42 +0000

    alalalal

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to Club Cloud's Application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1 class=\"p-3 mb-2 bg-primary text-white text-center\">Welcome to Club Cloud's Application</h1>"));
         return done();
       });
 
